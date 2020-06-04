4bfb66e102a00e8aa6b8c8d212e6bfa560d9ff7d
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-04 14:22:27 -0700

    made a basic schema for artists

--- app.js
+++ app.js
@@ -4,3 +4,18 @@
 // PATCH /resources/:id
 // DELETE /resources/:id
 
+//express lets us do the post/get/etc. 
+// patch updates something 
+
+const express = require('express'); 
+const app = express(); 
+const Shareable = require('./models/Shareable');
+
+app.use(require('cors')());
+app.use(express.json());
+
+app.post('./shareables', (req, res) => {
+    Shareable
+        .create(req.body)
+        .then(shareable => res.send(shareable));
+});
